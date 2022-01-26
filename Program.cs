e8565adcb086ed8506048ed9534385d97b88bd17
Mikita Kazlouski <kvipntv@gmail.com>
2022-01-26 12:21:43 +0300

    change object creating

--- Program.cs
+++ Program.cs
@@ -8,7 +8,25 @@ namespace main
     {
         public static void Main()
         {
-            
+            List<Goods> goodsList = new List<Goods>();
+
+            Storage.AddElementAssortment(new Snack("Pringls", 5.5), 12);
+            Storage.DeleteElementAssortment(new Snack(" ", 1));
+            Storage.Show();
+        }
+
+        public static void AddElementGoodList()
+        {
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "Beverage": break;
+                case "Snack": break;
+                case "ChocolatBar": break;
+                default: break;
+            }
+
         }
     }
 }
--- storage/Storage.cs
+++ storage/Storage.cs
@@ -7,10 +7,35 @@ using System.Threading.Tasks;
 namespace SmallTeachShop
 {
     internal static class Storage
-    {                    
+    {
+        static Dictionary<Goods, int> assortment = new Dictionary<Goods, int>();
+        static public void AddElementAssortment(Goods goods, int count)
+        {
+            assortment.Add(goods, count);
+        }
+        static public void DeleteElementAssortment(Goods goods)
+        {
+            
+        }
+
+        //static private bool CheckMethod(Goods goods)
+        //{
+        //    foreach ( var item in assortment)
+        //    {
+        //        if(item.Key.Titel == goods.Titel && item.Key.Price==goods.Price)
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+        //}
+
         static public void Show()
         {
-               
+            foreach (var item in assortment)
+            {
+                Console.WriteLine($"{item.Key.ToString()} - {item.Value}");
+            }
         }
     }
 }
